adb310e8a66f980d1c069eb92eb98af0c5dec5a8
AganRun <1379154067@qq.com>
2020-09-25 08:45:29 +0800

    feat(@Resource @Inject) 注解基本使用

--- spring-annoation/src/main/java/com/agan/service/PersonServiceImpl.java
+++ spring-annoation/src/main/java/com/agan/service/PersonServiceImpl.java
@@ -5,14 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
 /**
  * @author agan
  */
 @Service
-public class PersonServiceImpl implements PersonService{
+public class PersonServiceImpl implements PersonService {
 
-//    @Qualifier("personDao1")    //指定ID装配
-    @Autowired(required = false)      //默认按类型，其次按属性名
+    //    @Qualifier("personDao1")    //指定ID装配
+//    @Autowired(required = false)      //默认按类型，其次按属性名
+//    public PersonDao personDao2;
+
+//    @Resource(name = "personDao1")       //JAVA规范，默认按属性名，可以指定名称，不支持@Primary
+    @Inject     //支持@Primary,支持Spring的装配注解
     public PersonDao personDao2;
 
     @Override
